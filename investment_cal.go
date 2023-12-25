c87d2c1215011df5f80318d3fb54d9a6acedaef7
theycallmejoly <yking9187@gmail.com>
2023-12-25 15:47:57 +0330

    investment_calculator

---
+++ investment_cal.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const inflationRate = 2.5
+	var investmentAmount float64
+	var ExpectedReturnRate float64
+	var years int
+
+	fmt.Print("investment Amount: ")
+	fmt.Scan(&investmentAmount)
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&ExpectedReturnRate)
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	futurevalue := float64(investmentAmount) * math.Pow((1+ExpectedReturnRate/100), float64(years))
+	futureRealValue := futurevalue / math.Pow(1+inflationRate/100, float64(years))
+
+	fmt.Println(futurevalue)
+	fmt.Println(futureRealValue)
+}
